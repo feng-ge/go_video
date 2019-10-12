87e9cf7c60110d74c78344190209119b4cbb6362
feng-ge <zsf-forwork@outlook.com>
2019-10-12 11:51:55 +0800

    v1.0

--- video_server/build.sh
+++ video_server/build.sh
@@ -4,4 +4,4 @@
 cd ~/go/src/go_video/video_server/web/
 go install
 cp ~/go/bin/web ~/go/bin/video_server_web_ui/web
-cp -R ~/go/src/golang-streaming/video_server/templates ~/go/bin/video_server_web_ui/web
+cp -R ~/go/src/go_video/video_server/templates ~/go/bin/video_server_web_ui/web
